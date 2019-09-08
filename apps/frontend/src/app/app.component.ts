ea5090784fca6722d07878c784b024556277417b
Jesse Johnson <Jesse.Johnson@altsrc.net>
2019-09-07 17:47:30 -0700

    Add the monorepo

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'holahola-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'frontend';
+}
