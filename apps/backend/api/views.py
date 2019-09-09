3d68385ae61aa031361379069044fa9d2b98a971
Marshall Powell <powellmarshall3@gmail.com>
2019-09-09 12:57:54 -0700

    remove duplicate backend directory

--- apps/backend/api/views.py
+++ apps/backend/api/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from rest_framework import viewsets
 from .models import Language, Holero
 from .serializers import HoleroSerializer, LanguageSerializer
 
-
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 
 class HoleroViewSet(viewsets.ModelViewSet):
     queryset = Holero.objects.all()
