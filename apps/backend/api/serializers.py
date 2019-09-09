84b73d61bc1b7a67abe4862db3c8ec21114752ab
Marshall Powell <marshall.powell@altsrc.net>
2019-09-09 15:40:51 -0700

    remove swagger

--- apps/backend/api/serializers.py
+++ apps/backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Language, Holero
+from django.contrib.auth.models import User, Group
 
 
 class HoleroSerializer(serializers.HyperlinkedModelSerializer):
@@ -12,3 +13,15 @@ class LanguageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Language
         fields = '__all__'
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = ['url', 'name']
