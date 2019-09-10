09ff7d349e88ab7646752e682a58d774c572caae
Marshall Powell <powellmarshall3@gmail.com>
2019-09-09 22:43:46 -0700

    update models

--- apps/backend/api/models.py
+++ apps/backend/api/models.py
@@ -14,6 +14,9 @@ class Language(models.Model):
     def __unicode__(self):
         return self.name
 
+    def __str__(self):
+        return self.name
+
 
 class Holero(models.Model):
     first_name = models.CharField(max_length=50)
@@ -25,4 +28,7 @@ class Holero(models.Model):
         verbose_name_plural = "Holeros"
 
     def __unicode__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
+    def __str__(self):
         return '%s %s' % (self.first_name, self.last_name)
